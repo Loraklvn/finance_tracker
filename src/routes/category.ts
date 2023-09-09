c88dd32164017b64b26b8e23bc8f8c116169b4aa
Loraklvn <68545884+Loraklvn@users.noreply.github.com>
2023-09-09 09:56:19 -0400

    :sparkles: add fields validation

--- src/routes/category.ts
+++ src/routes/category.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { PATH_CATEGORY } from '../constants/paths';
 import { createUserCategory, getCategories } from '../controllers/category';
+import validateFields from '../middlewares/validateFields';
 
 const categoryRouter: Router = Router();
 
 categoryRouter.get(PATH_CATEGORY, getCategories);
 
-categoryRouter.post(`${PATH_CATEGORY}/user`, createUserCategory);
+categoryRouter.post(
+  `${PATH_CATEGORY}/user`,
+  validateFields([
+    body('value').isString().notEmpty(),
+    body('description').isString().notEmpty(),
+    body('type').isString().notEmpty(),
+  ]),
+  createUserCategory,
+);
 
 export default categoryRouter;
--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -1,10 +1,59 @@
 import { Router } from 'express';
-import { createTransaction, getTransactions } from '../controllers/transaction';
+import {
+  createTransaction,
+  deleteTransaction,
+  getTransactions,
+  updateTransaction,
+} from '../controllers/transaction';
+import validateFields from '../middlewares/validateFields';
+import { body } from 'express-validator';
+import { TransactionTypes } from '../types/transaction';
+import { PATH_TRANSACTION } from '../constants/paths';
 
 const transactionRouter: Router = Router();
 
-transactionRouter.get('/transactions', getTransactions);
+transactionRouter.get(
+  PATH_TRANSACTION,
+  validateFields([
+    body('page').optional().isInt(),
+    body('pageSize').optional().isInt(),
+    body('startDate').optional().isDate(),
+    body('endDate').optional().isDate(),
+  ]),
+  getTransactions,
+);
 
-transactionRouter.post('/transactions', createTransaction);
+const validateTypeEnum = (value: TransactionTypes) => {
+  if (!Object.values(TransactionTypes).includes(value)) {
+    throw new Error('Invalid type value');
+  }
+  return true;
+};
+
+transactionRouter.post(
+  PATH_TRANSACTION,
+  validateFields([
+    body('amount').isNumeric().notEmpty(),
+    body('note').optional().isString().notEmpty(),
+    body('type').isString().notEmpty().custom(validateTypeEnum),
+    body('category_id').isNumeric().notEmpty(),
+    body('date').isISO8601(),
+  ]),
+  createTransaction,
+);
+
+transactionRouter.put(
+  `${PATH_TRANSACTION}/:id`,
+  validateFields([
+    body('amount').optional().isNumeric().notEmpty(),
+    body('note').optional().isString().notEmpty(),
+    body('type').optional().isString().notEmpty().custom(validateTypeEnum),
+    body('category_id').optional().isNumeric().notEmpty(),
+    body('date').optional().isISO8601(),
+  ]),
+  updateTransaction,
+);
+
+transactionRouter.delete(`${PATH_TRANSACTION}/:id`, deleteTransaction);
 
 export default transactionRouter;
