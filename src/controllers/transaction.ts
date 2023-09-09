24ecf8468cd8f4e17361d618916ef44a7b78bf0d
Loraklvn <68545884+Loraklvn@users.noreply.github.com>
2023-09-09 15:24:25 -0400

    :goal_net: catch errors inside controllers

--- src/controllers/transaction.ts
+++ src/controllers/transaction.ts
@@ -17,135 +17,163 @@ import { TransactionTypes } from '../types/transaction';
 export const getTransactions = async (
   req: CustomRequest,
   res: Response,
+  next: NextFunction,
 ): Promise<void> => {
-  const userId = req?.user?.user_id as string;
-  const { page = 1, pageSize = MAX_PAGE_SIZE, startDate, endDate } = req.query;
+  try {
+    const userId = req?.user?.user_id as string;
+    const {
+      page = 1,
+      pageSize = MAX_PAGE_SIZE,
+      startDate,
+      endDate,
+    } = req.query;
 
-  const parsedPage = parseInt(page as string);
-  const parsedPageSize = parseInt(pageSize as string);
-  const validatedPageSize = Math.min(parsedPageSize, MAX_PAGE_SIZE);
+    const parsedPage = parseInt(page as string);
+    const parsedPageSize = parseInt(pageSize as string);
+    const validatedPageSize = Math.min(parsedPageSize, MAX_PAGE_SIZE);
 
-  const transactionRepository = appDataSource.getRepository(Transaction);
+    const transactionRepository = appDataSource.getRepository(Transaction);
 
-  const transactionsQuery = buildGetTransactionsQuery(transactionRepository, {
-    userId,
-    page: parsedPage,
-    pageSize: validatedPageSize,
-    startDate: startDate as string,
-    endDate: endDate as string,
-  });
-  const [transactions, total] = await transactionsQuery.getManyAndCount();
+    const transactionsQuery = buildGetTransactionsQuery(transactionRepository, {
+      userId,
+      page: parsedPage,
+      pageSize: validatedPageSize,
+      startDate: startDate as string,
+      endDate: endDate as string,
+    });
+    const [transactions, total] = await transactionsQuery.getManyAndCount();
 
-  const summaryQuery = buildGetTransactionsSummaryQuery(transactionRepository, {
-    userId,
-    startDate: startDate as string,
-    endDate: endDate as string,
-  });
-  const summary = await summaryQuery.getRawOne();
+    const summaryQuery = buildGetTransactionsSummaryQuery(
+      transactionRepository,
+      {
+        userId,
+        startDate: startDate as string,
+        endDate: endDate as string,
+      },
+    );
+    const summary = await summaryQuery.getRawOne();
 
-  res.json({
-    status: HTTP_STATUS.SUCCESS,
-    data: {
-      total,
-      pageSize: parsedPageSize,
-      currentPage: parsedPage,
-      totalPages: parseInt(`${Math.ceil(total / validatedPageSize)}`),
-      ...summary,
-      transactions,
-    },
-  });
+    res.json({
+      status: HTTP_STATUS.SUCCESS,
+      data: {
+        total,
+        pageSize: parsedPageSize,
+        currentPage: parsedPage,
+        totalPages: parseInt(`${Math.ceil(total / validatedPageSize)}`),
+        ...summary,
+        transactions,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const createTransaction = async (
   req: CustomRequest,
   res: Response,
+  next: NextFunction,
 ): Promise<void> => {
-  const userId = req?.user?.user_id as string;
+  try {
+    const userId = req?.user?.user_id as string;
 
-  const { amount, note, type, category_id: categoryID, date } = req.body;
+    const { amount, note, type, category_id: categoryID, date } = req.body;
 
-  const transaction = await Transaction.create({
-    amount,
-    note,
-    type,
-    category_id: categoryID,
-    user_id: parseInt(userId),
-    date,
-  }).save();
+    const transaction = await Transaction.create({
+      amount,
+      note,
+      type,
+      category_id: categoryID,
+      user_id: parseInt(userId),
+      date,
+    }).save();
 
-  res.json({
-    status: HTTP_STATUS.SUCCESS,
-    data: {
-      transaction,
-    },
-  });
+    res.json({
+      status: HTTP_STATUS.SUCCESS,
+      data: {
+        transaction,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const updateTransaction = async (
   req: CustomRequest,
   res: Response,
+  next: NextFunction,
 ): Promise<void> => {
-  const userId = req?.user?.user_id as string;
-  const { id } = req.params;
+  try {
+    const userId = req?.user?.user_id as string;
+    const { id } = req.params;
 
-  const transaction = await Transaction.findOneBy({
-    transaction_id: parseInt(id),
-    user_id: parseInt(userId),
-  });
-
-  if (!transaction) {
-    res.status(HTTP_STATUS_CODE.NOT_FOUND).json({
-      status: HTTP_STATUS.ERROR,
-      message: ERROR_MESSAGES.TRANSACTION_NOT_FOUND,
+    const transaction = await Transaction.findOneBy({
+      transaction_id: parseInt(id),
+      user_id: parseInt(userId),
     });
-    return;
-  }
 
-  TRANSACTION_ALLOWED_UPDATE_FIELDS.forEach((field) => {
-    if (field in req.body) {
-      transaction[field as 'amount'] = req.body[field];
+    if (!transaction) {
+      res.status(HTTP_STATUS_CODE.NOT_FOUND).json({
+        status: HTTP_STATUS.ERROR,
+        message: ERROR_MESSAGES.TRANSACTION_NOT_FOUND,
+      });
+      return;
     }
-  });
 
-  await transaction.save();
+    TRANSACTION_ALLOWED_UPDATE_FIELDS.forEach((field) => {
+      if (field in req.body) {
+        transaction[field as 'amount'] = req.body[field];
+      }
+    });
 
-  res.json({
-    status: HTTP_STATUS.SUCCESS,
-    data: {
-      transaction,
-    },
-  });
+    await transaction.save();
+
+    res.json({
+      status: HTTP_STATUS.SUCCESS,
+      data: {
+        transaction,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const deleteTransaction = async (
   req: CustomRequest,
   res: Response,
+  next: NextFunction,
 ): Promise<void> => {
-  const userId = req?.user?.user_id as string;
+  try {
+    const userId = req?.user?.user_id as string;
 
-  const { id } = req.params;
+    const { id } = req.params;
 
-  const transaction = await Transaction.findOneBy({
-    transaction_id: parseInt(id),
-    user_id: parseInt(userId),
-  });
-
-  if (!transaction) {
-    res.status(HTTP_STATUS_CODE.NOT_FOUND).json({
-      status: HTTP_STATUS.ERROR,
-      message: ERROR_MESSAGES.TRANSACTION_NOT_FOUND,
+    const transaction = await Transaction.findOneBy({
+      transaction_id: parseInt(id),
+      user_id: parseInt(userId),
     });
-    return;
+
+    if (!transaction) {
+      res.status(HTTP_STATUS_CODE.NOT_FOUND).json({
+        status: HTTP_STATUS.ERROR,
+        message: ERROR_MESSAGES.TRANSACTION_NOT_FOUND,
+      });
+      return;
+    }
+
+    await transaction.remove();
+
+    res.json({
+      status: HTTP_STATUS.SUCCESS,
+      data: {
+        transaction,
+      },
+    });
+  } catch (error) {
+    next(error);
   }
-
-  await transaction.remove();
-
-  res.json({
-    status: HTTP_STATUS.SUCCESS,
-    data: {
-      transaction,
-    },
-  });
 };
 
 export const getTransactionsSummary = async (
